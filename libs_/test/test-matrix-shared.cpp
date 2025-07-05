69366b940c9c09afcbe285b6c662443d65f0c7f2
adeptim <adeptpinaev@yandex.ru>
2025-07-05 22:20:31 +0600

    lib

--- libs_/test/test-matrix-shared.cpp
+++ libs_/test/test-matrix-shared.cpp
@@ -7,8 +7,14 @@ extern "C"
 #endif
     extern Matrix MATRIXLIB_EXPORT *math_createMatrix(int, int);
     extern void MATRIXLIB_EXPORT math_deleteMatrix(Matrix *);
-    extern double MATRIXLIB_EXPORT math_get(Matrix *, int, int);
-    extern void MATRIXLIB_EXPORT math_set(Matrix *, int, int, double);
+    extern float MATRIXLIB_EXPORT math_get(Matrix *, int, int);
+    extern void MATRIXLIB_EXPORT  math_set(Matrix *, int, int, float);
+    extern Matrix  MATRIXLIB_EXPORT *math_add(const Matrix *A, const Matrix *B);
+extern Matrix  MATRIXLIB_EXPORT *math_subtract(const Matrix *A, const Matrix *B);
+extern Matrix  MATRIXLIB_EXPORT *math_dot(const Matrix *A, const Matrix *B);
+extern Matrix  MATRIXLIB_EXPORT *math_add_(const Matrix *A, const Matrix *B);
+extern Matrix   MATRIXLIB_EXPORT *math_subtract_(const Matrix *A, const Matrix *B);
+extern Matrix  MATRIXLIB_EXPORT *math_dot_(const Matrix *A, int k);
     extern void MATRIXLIB_EXPORT math_print(Matrix *);
 #ifdef __cplusplus
 }
@@ -16,18 +22,27 @@ extern "C"
 
 int main()
 {
-    // не работает, т.к. к проекту подключена динамическая бибилиотека
-    // math::Matrix m(3, 3);
-    // m(0,0) = 1.;
-    // m(1,1) = 1.;
+   
+    
+   Matrix *m1 = math_createMatrix(3, 3);
+  math_set(m1,0,0,1.);
+    math_set(m1,1,1,1.);
+  //  math_print(m1);
+     Matrix *m2 = math_createMatrix(3, 3);
+  math_set(m2,0,0,2.);
+    math_set(m2,1,1,1.);
+ Matrix *m3 = math_createMatrix(3, 3);
+m3=math_add(m1,m2);
+//math_print(m3);
+m3=math_subtract(m1,m2);
+math_print(m3);
+//m3=math_dot(m1,m2);
+m3=math_add_(m3,m1);
 
-    // работает
-    Matrix *m1 = math_createMatrix(3, 3);
-
-    math_set(m1,1,1,1);
-    math_set(m1,1,1,1);
-    math_print(m1);
-
+//m3=math_dot_(m1,3);
+math_print(m3);
+Matrix *m4 = math_createMatrix(3, 3);
+math_print(m4);
     // в конце обязательно вызываем деструктор
     math_deleteMatrix(m1);
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,15 @@ extern "C"
 #endif
     extern Matrix __declspec(dllimport) *math_createMatrix(int, int);
     extern void __declspec(dllimport) math_deleteMatrix(Matrix *);
-    extern double __declspec(dllimport) math_get(Matrix *, int, int);
-    extern void __declspec(dllimport) math_set(Matrix *, int, int, int);
+    extern double __declspec(dllimport) math_get(const Matrix *, int, int);
+    extern void __declspec(dllimport) math_set(Matrix *, int, int, double);
     extern void __declspec(dllimport) math_print(Matrix *);
-
-
-
+extern Matrix __declspec(dllimport) *math_add(const Matrix *A, const Matrix *B);
+extern Matrix __declspec(dllimport) *math_subtract(const Matrix *A, const Matrix *B);
+extern Matrix __declspec(dllimport) *math_dot(const Matrix *A, const Matrix *B);
+extern Matrix  __declspec(dllimport) *math_add_(const Matrix *A, const Matrix *B);
+extern Matrix  __declspec(dllimport) *math_subtract_(const Matrix *A, const Matrix *B);
+extern Matrix  __declspec(dllimport)   *math_dot_(const Matrix *A, int k);
 
 #ifdef __cplusplus
 }
@@ -24,10 +27,21 @@ int main()
 {
   
    Matrix *m1 = math_createMatrix(3, 3);
+  math_set(m1, 0, 0, 1.);
+    math_set(m1, 1, 1, 1.);
+   // math_print(m1);
+  Matrix *m2 = math_createMatrix(3, 3);
+  math_set(m2, 0, 0, 2.);
+    math_set(m2, 1, 1, 2.);
+   math_print(m2);
+  Matrix *m3 = math_createMatrix(3, 3);
+m3=math_add(m1,m2);
+m3=math_subtract(m1,m2);
+m3=math_dot(m1,m2);
+math_print(m3);
+m3=math_dot_(m2,2);
+math_print(m3);
 
-    math_set(m1, 0, 0, 1);
-    math_set(m1, 1, 1, 1);
-   math_print(m1);
  std::cout <<"vvv";
      std::cout << std::endl;
     return 0;
